a60fba084415aa7e0470fdeea084b5161c2dae39
siwanon.mun <siwanon.mun@mail.mfec.co.th>
2018-03-14 18:03:31 +0700

    change scss to import only one file

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,8 @@ gulp.task("js", function() {
     .src([
       "node_modules/bootstrap/dist/js/bootstrap.min.js",
       "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/popper.js/dist/umd/popper.min.js"
+      "node_modules/popper.js/dist/umd/popper.min.js",
+      "src/**/*.js"
     ])
     .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
