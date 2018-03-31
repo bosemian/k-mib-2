f6ff531c67d5f0b79709c6b88960f4fb39e32caf
siwanon.mun <siwanon.mun@mail.mfec.co.th>
2018-03-31 12:34:26 +0700

    add social button

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,25 @@ const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
 const minifyHTML = require("gulp-minify-html");
 
+const devPath = "./src"
+const prodPath = "./dist"
+
 // Compile Sass & Inject Into Browser
 gulp.task("sass", function() {
   return gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
     .pipe(sass())
     .pipe(cleanCSS())
-    .pipe(gulp.dest("./dist/css"))
+    .pipe(gulp.dest(`${devPath}/css`))
     .pipe(browserSync.stream());
 });
 
-gulp.task("html", function() {
-  return gulp
-    .src("src/*.html")
-    .pipe(minifyHTML())
-    .pipe(gulp.dest("./dist"));
-});
+// gulp.task("html", function() {
+//   return gulp
+//     .src("src/*.html")
+//     .pipe(minifyHTML())
+//     .pipe(gulp.dest(devPath));
+// });
 
 // Move JS Files to src/js
 gulp.task("js", function() {
@@ -30,7 +33,7 @@ gulp.task("js", function() {
       "node_modules/jquery/dist/jquery.min.js",
       "node_modules/popper.js/dist/umd/popper.min.js"
     ])
-    .pipe(gulp.dest("./dist/js"))
+    .pipe(gulp.dest(`${devPath}/js`))
     .pipe(sourcemaps.write())
     .pipe(browserSync.stream());
 });
@@ -39,26 +42,26 @@ gulp.task("js", function() {
 gulp.task("fonts", function() {
   return gulp
     .src("node_modules/font-awesome/fonts/*")
-    .pipe(gulp.dest("./dist/fonts"));
+    .pipe(gulp.dest(`${devPath}/fonts`));
 });
 
 // Move Font Awesome CSS to src/css
 gulp.task("fa", function() {
   return gulp
     .src("node_modules/font-awesome/css/font-awesome.min.css")
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(gulp.dest(`${devPath}/css`));
 });
 
 gulp.task("images", function() {
-  return gulp.src(["src/images/**/*"]).pipe(gulp.dest("./dist/images"));
+  return gulp.src(["src/images/**/*"]).pipe(gulp.dest(`${devPath}/images`));
 });
 
-gulp.task("default", ["sass", "html", "js", "serve", "fa", "fonts", "images"]);
+gulp.task("default", ["sass", "js", "serve", "fa", "fonts", "images"]);
 
 // Watch Sass & Serve
 gulp.task("serve", ["sass"], function() {
   browserSync.init({
-    server: "./src"
+    server: devPath
   });
 
   gulp.watch(
